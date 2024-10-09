ef5662bbcf8488614607496f758ed2880b2731a9
meili555 <135024085+meili555@users.noreply.github.com>
2024-10-09 22:33:15 +0800

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -17,7 +17,7 @@ setInterval(function () {
 
 
 document.oncontextmenu = function () { return false; };    
- var x='https://link.zhihu.com/?target=http://点击继续访问.dalong.cf/?';
+ var x='https://link.zhihu.com/?target=https://点击继续访问.dalong.cf/?fr';
     $(function () {
         let u = window.document.location.href.toString().split("?");
         if (typeof (u[1]) == "string") {
